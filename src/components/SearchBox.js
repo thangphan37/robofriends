12aec751aadd0a58cbfe0b04559e352ad51c60c2
thangphan37 <pcthangk59@gmail.com>
2020-09-30 06:39:59 +0900

    Improve performance

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,8 @@ import React from 'react';
 function SearchBox({onSearchRobots}) {
   return(
     <div className="pa2">
-      <input type="search" className="pa3 ba b--green bg-lightest-blue" onChange={onSearchRobots} placeholder="Search robot"/>
+      <label for="robo"></label>
+      <input id="robo" type="search" className="pa3 ba b--green bg-lightest-blue" onChange={onSearchRobots} placeholder="Search robot"/>
     </div>
   )
 }
