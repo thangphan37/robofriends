9ec8bc6f949187f0a0b6cdfdad1464a0c846f47c
thangphan37 <pcthangk59@gmail.com>
2020-09-30 10:07:04 +0900

    Add icon

--- src/components/SearchBox.js
+++ src/components/SearchBox.js
@@ -3,7 +3,9 @@ import React from 'react';
 function SearchBox({onSearchRobots}) {
   return(
     <div className="pa2">
-      <label for="robo"></label>
+      <label htmlFor="robo" style={{color: '#FFFFFF', padding: '2px'}}>
+        Let's type:
+      </label>
       <input id="robo" type="search" className="pa3 ba b--green bg-lightest-blue" onChange={onSearchRobots} placeholder="Search robot"/>
     </div>
   )
